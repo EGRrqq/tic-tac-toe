f15f864ee365a7dac7dee1f79e3bc9b9298761d6
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-09-27 20:24:16 -0500

    feat(js): grid fill for tic tac toe game
    feat(html): basic markup for the game
    feat(css):
      - basic styles for btn
      - centered gameboard

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("yo");
+const gameboard = (() => {
+  const gameContent = [];
+
+  const gameNode = (btn) => {
+    const link = btn;
+    const textContent = btn.textContent;
+
+    return {
+      link,
+      textContent,
+    };
+  };
+
+  function getBtns() {
+    const buttons = document.querySelectorAll(".screen > button");
+
+    buttons.forEach((btn) => {
+      gameContent.push(gameNode(btn));
+    });
+  }
+
+  function fillBtns() {
+    for (let node of gameContent) {
+      const randomMark = ["X", "O"][Math.floor(Math.random() * 2)];
+
+      node.textContent = randomMark;
+    }
+  }
+
+  function updateBtns() {
+    for (let node of gameContent) {
+      node.link.textContent = node.textContent;
+    }
+  }
+
+  (function _render() {
+    getBtns();
+    fillBtns();
+    updateBtns();
+  })();
+
+  return {
+    gameContent,
+  };
+})();
