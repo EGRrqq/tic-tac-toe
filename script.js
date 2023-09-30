90c70d7db16ac3295f44bfcbf1dc59247965b234
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-09-29 19:49:30 -0500

    feat(js): add the ability to play the game
    - add playerId to the gameNode factory
    - - add get/set methods for playerId
    
    - create a gamePlayer factory
    - create a gameController

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const gameBoard = (function () {
 
   for (let i = 0; i < rows; i++) {
     board[i] = [];
+
     for (let j = 0; j < columns; j++) {
       board[i].push(gameNode());
     }
@@ -12,24 +13,7 @@ const gameBoard = (function () {
 
   const getBoard = () => board;
 
-  const makeMark = (row, column, choice) => {
-    // The main idea is to use this big ass condition for rows and columns:
-    // - Other conditions lose the `choice` return statement.
-    // - Row and column return messages appears only after the `choice` value is typed.
-
-    if (row !== 0 && row !== 1 && row !== 2)
-      return "available row values: 0, 1, 2";
-
-    if (column !== 0 && column !== 1 && column !== 2)
-      return "available column values: 0, 1, 2";
-
-    if (parseInt(choice) !== 0 && parseInt(choice) !== 1)
-      return "available choice values: 0, 1";
-
-    if (board[row][column].getMark() !== "") return "cell is full";
-
-    board[row][column].pickMark(choice);
-  };
+  const makeMark = (row, column, choice) => board[row][column].pickMark(choice);
 
   const printBoardValues = () => {
     const boardValues = gameBoard
@@ -48,6 +32,7 @@ const gameBoard = (function () {
 
 function gameNode() {
   let mark = "";
+  let playerId = "";
 
   const getMark = () => mark;
 
@@ -57,5 +42,94 @@ function gameNode() {
     mark = marks[choice];
   };
 
-  return { getMark, pickMark };
+  const setPlayerId = (playerName) => (playerId = playerName);
+  const getPlayerId = () => playerId;
+
+  return {
+    getMark,
+    pickMark,
+    setPlayerId,
+    getPlayerId,
+  };
 }
+
+function gamePlayer(initName) {
+  let name = initName;
+  const gameHistory = [];
+
+  const getName = () => name;
+  const changeName = (newName) => (name = newName);
+
+  const addToGameHistory = (row, column) =>
+    gameHistory.push(gameBoard.getBoard()[row][column].getMark());
+
+  const resetPlayerHistory = () => gameHistory.splice(0, gameHistory.length);
+  const getFullHistory = () => gameHistory;
+  const getLastHistory = () => gameHistory[gameHistory.length - 1];
+
+  return {
+    getName,
+    changeName,
+    addToGameHistory,
+    resetPlayerHistory,
+    getFullHistory,
+    getLastHistory,
+  };
+}
+
+const gameController = (function () {
+  const players = [gamePlayer("juh"), gamePlayer("bluh")];
+  let activePlayer = players[0];
+
+  const switchActivePlayer = () =>
+    (activePlayer = activePlayer === players[0] ? players[1] : players[0]);
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    console.log("Current board state: ", gameBoard.printBoardValues());
+    console.log(`Player ${getActivePlayer().getName()} turn.`);
+  };
+
+  const playRound = (row, column, choice) => {
+    // Why are you using these big ass conditions to validate rows and columns?
+    // - Other conditions lose the `choice` return statement.
+    // - Row and column return messages appears only after the `choice` value is typed.
+    //
+    // Why not put this validation in a separate function?
+    // - I want to have some kind of validation "on type" and on submit.
+    // - If I move the validation to a separate function, I will only have validation on submit.
+    // - It`s possible to make a separate validation function with a separate console.log that will contain all the requirements, but I don't want to do that
+
+    if (row !== 0 && row !== 1 && row !== 2)
+      return "available row values: 0, 1, 2";
+
+    if (column !== 0 && column !== 1 && column !== 2)
+      return "available column values: 0, 1, 2";
+
+    if (parseInt(choice) !== 0 && parseInt(choice) !== 1)
+      return "available choice values: 0 for X, 1 for O";
+
+    if (gameBoard.getBoard()[row][column].getMark() !== "")
+      return "cell is full";
+
+    gameBoard.getBoard()[row][column].setPlayerId(getActivePlayer().getName());
+
+    gameBoard.makeMark(row, column, choice);
+    getActivePlayer().addToGameHistory(row, column);
+
+    console.log(
+      `Player ${getActivePlayer().getName()} place ${getActivePlayer().getLastHistory()} mark to (${row}, ${column}) `,
+    );
+
+    switchActivePlayer();
+    printNewRound();
+  };
+
+  console.log("Current board state: ", gameBoard.printBoardValues());
+
+  return {
+    playRound,
+    getActivePlayer,
+  };
+})();
