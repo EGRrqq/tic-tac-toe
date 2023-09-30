ccb6985b95db59583b0555d64edee80eac1ebf67
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-09-29 22:18:28 -0500

    feat(js): encapsulate all data inside a game func
    feat(js): add console logs to separate output

--- script.js
+++ script.js
@@ -1,135 +1,151 @@
-const gameBoard = (function () {
-  const rows = 3;
-  const columns = 3;
-  const board = [];
+const game = (function () {
+  const gameBoard = (function () {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
 
-    for (let j = 0; j < columns; j++) {
-      board[i].push(gameNode());
+      for (let j = 0; j < columns; j++) {
+        board[i].push(gameNode());
+      }
     }
+
+    const getBoard = () => board;
+
+    const makeMark = (row, column, choice) =>
+      board[row][column].pickMark(choice);
+
+    const printBoardValues = () => {
+      const boardValues = gameBoard
+        .getBoard()
+        .map((row) => row.map((node) => node.getMark()));
+
+      return boardValues;
+    };
+
+    return {
+      getBoard,
+      printBoardValues,
+      makeMark,
+    };
+  })();
+
+  function gameNode() {
+    let mark = "";
+    let playerId = "";
+
+    const getMark = () => mark;
+
+    const pickMark = (choice) => {
+      const marks = ["X", "O"];
+
+      mark = marks[choice];
+    };
+
+    const setPlayerId = (playerName) => (playerId = playerName);
+    const getPlayerId = () => playerId;
+
+    return {
+      getMark,
+      pickMark,
+      setPlayerId,
+      getPlayerId,
+    };
   }
 
-  const getBoard = () => board;
+  function gamePlayer(initName) {
+    let name = initName;
+    const gameHistory = [];
 
-  const makeMark = (row, column, choice) => board[row][column].pickMark(choice);
+    const getName = () => name;
+    const changeName = (newName) => (name = newName);
 
-  const printBoardValues = () => {
-    const boardValues = gameBoard
-      .getBoard()
-      .map((row) => row.map((node) => node.getMark()));
+    const addToGameHistory = (row, column) =>
+      gameHistory.push(gameBoard.getBoard()[row][column].getMark());
 
-    return boardValues;
-  };
+    const resetPlayerHistory = () => gameHistory.splice(0, gameHistory.length);
+    const getFullHistory = () => gameHistory;
+    const getLastHistory = () => gameHistory[gameHistory.length - 1];
 
-  return {
-    getBoard,
-    printBoardValues,
-    makeMark,
-  };
-})();
+    return {
+      getName,
+      changeName,
+      addToGameHistory,
+      resetPlayerHistory,
+      getFullHistory,
+      getLastHistory,
+    };
+  }
 
-function gameNode() {
-  let mark = "";
-  let playerId = "";
+  const gameController = (function () {
+    const players = [gamePlayer("juh"), gamePlayer("bluh")];
+    let activePlayer = players[0];
 
-  const getMark = () => mark;
+    const switchActivePlayer = () =>
+      (activePlayer = activePlayer === players[0] ? players[1] : players[0]);
 
-  const pickMark = (choice) => {
-    const marks = ["X", "O"];
+    const getActivePlayer = () => activePlayer;
 
-    mark = marks[choice];
-  };
+    const printNewRound = () => {
+      console.log("Current board state: ", gameBoard.printBoardValues());
+      console.log(`Player ${getActivePlayer().getName()} turn.`);
+      console.log("----------------");
+    };
 
-  const setPlayerId = (playerName) => (playerId = playerName);
-  const getPlayerId = () => playerId;
+    const playRound = (row, column, choice) => {
+      // Why are you using these big ass conditions to validate rows and columns?
+      // - Other conditions lose the `choice` return statement.
+      // - Row and column return messages appears only after the `choice` value is typed.
+      //
+      // Why not put this validation in a separate function?
+      // - I want to have some kind of validation "on type" and on submit.
+      // - If I move the validation to a separate function, I will only have validation on submit.
+      // - It`s possible to make a separate validation function with a separate console.log that will contain all the requirements, but I don't want to do that
 
-  return {
-    getMark,
-    pickMark,
-    setPlayerId,
-    getPlayerId,
-  };
-}
+      if (row !== 0 && row !== 1 && row !== 2)
+        return "available row values: 0, 1, 2";
 
-function gamePlayer(initName) {
-  let name = initName;
-  const gameHistory = [];
+      if (column !== 0 && column !== 1 && column !== 2)
+        return "available column values: 0, 1, 2";
 
-  const getName = () => name;
-  const changeName = (newName) => (name = newName);
+      if (parseInt(choice) !== 0 && parseInt(choice) !== 1)
+        return "available choice values: 0 for X, 1 for O";
 
-  const addToGameHistory = (row, column) =>
-    gameHistory.push(gameBoard.getBoard()[row][column].getMark());
+      if (gameBoard.getBoard()[row][column].getMark() !== "")
+        return "cell is full";
 
-  const resetPlayerHistory = () => gameHistory.splice(0, gameHistory.length);
-  const getFullHistory = () => gameHistory;
-  const getLastHistory = () => gameHistory[gameHistory.length - 1];
+      gameBoard
+        .getBoard()
+        [row][column].setPlayerId(getActivePlayer().getName());
 
-  return {
-    getName,
-    changeName,
-    addToGameHistory,
-    resetPlayerHistory,
-    getFullHistory,
-    getLastHistory,
-  };
-}
+      gameBoard.makeMark(row, column, choice);
+      getActivePlayer().addToGameHistory(row, column);
 
-const gameController = (function () {
-  const players = [gamePlayer("juh"), gamePlayer("bluh")];
-  let activePlayer = players[0];
+      console.log("----------------");
+      console.log(
+        `Player ${getActivePlayer().getName()} place ${getActivePlayer().getLastHistory()} mark to (${row}, ${column}) `,
+      );
 
-  const switchActivePlayer = () =>
-    (activePlayer = activePlayer === players[0] ? players[1] : players[0]);
+      switchActivePlayer();
+      printNewRound();
+    };
 
-  const getActivePlayer = () => activePlayer;
-
-  const printNewRound = () => {
+    console.log("----------------");
+    console.info("To play, use the game.playRound() method");
+    console.log("----------------");
     console.log("Current board state: ", gameBoard.printBoardValues());
-    console.log(`Player ${getActivePlayer().getName()} turn.`);
-  };
+    console.log("----------------");
 
-  const playRound = (row, column, choice) => {
-    // Why are you using these big ass conditions to validate rows and columns?
-    // - Other conditions lose the `choice` return statement.
-    // - Row and column return messages appears only after the `choice` value is typed.
-    //
-    // Why not put this validation in a separate function?
-    // - I want to have some kind of validation "on type" and on submit.
-    // - If I move the validation to a separate function, I will only have validation on submit.
-    // - It`s possible to make a separate validation function with a separate console.log that will contain all the requirements, but I don't want to do that
-
-    if (row !== 0 && row !== 1 && row !== 2)
-      return "available row values: 0, 1, 2";
-
-    if (column !== 0 && column !== 1 && column !== 2)
-      return "available column values: 0, 1, 2";
-
-    if (parseInt(choice) !== 0 && parseInt(choice) !== 1)
-      return "available choice values: 0 for X, 1 for O";
-
-    if (gameBoard.getBoard()[row][column].getMark() !== "")
-      return "cell is full";
-
-    gameBoard.getBoard()[row][column].setPlayerId(getActivePlayer().getName());
-
-    gameBoard.makeMark(row, column, choice);
-    getActivePlayer().addToGameHistory(row, column);
-
-    console.log(
-      `Player ${getActivePlayer().getName()} place ${getActivePlayer().getLastHistory()} mark to (${row}, ${column}) `,
-    );
-
-    switchActivePlayer();
-    printNewRound();
-  };
-
-  console.log("Current board state: ", gameBoard.printBoardValues());
+    return {
+      playRound,
+      getActivePlayer,
+    };
+  })();
 
   return {
-    playRound,
-    getActivePlayer,
+    playRound: gameController.playRound,
+    getActivePlayer: gameController.getActivePlayer,
   };
 })();
